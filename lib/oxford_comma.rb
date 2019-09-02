0927a59573dc1054b4cb7360ecd735a060ddc208
Jamal Frank <jamaljfrank@gmail.com>
2019-09-02 04:36:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,13 @@ def oxford_comma(array)
   end
 end
 
+# oxford_comma(array)
+  # if array.length <= 1
+    # array[0]
+  # elsif array.length == 2
+    # "#{array[0]} and #{array[1]}"
+  # else
+    # array[-1] = "and #{array[-1]}"
+    # array.join ", "
+  # end
+# end
